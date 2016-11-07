4d22612ed994f9f47d7d5e08605b9cb5dd993f5e
Richard Zhang <lapislacrima@gmail.com>
2016-11-07 17:40:36 +0000

    makes an asynchronous https request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
 
 console.log('I did it!');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+// this call is asynchronous
+https.request(options, callback).end();
+
+console.log("I've made the request!");
