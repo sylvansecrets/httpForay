7798ff27064a1217ca680ce432d08cfcac64046b
Richard Zhang <lapislacrima@gmail.com>
2016-11-07 17:43:42 +0000

    logs IncomingMesssage

--- https_example.js
+++ https_example.js
@@ -1,14 +1,13 @@
 var https = require('https');
 
-console.log('I did it!');
-
 var options = {
   host: 'www.example.org',
   path: '/'
 };
 
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response: ', response);
 }
 
 console.log("I'm about to make the request!");
